c3add9e03d4a7bd68ad7275235d27ab3f9da23d4
Raphael <19925795+RaphEnio@users.noreply.github.com>
2020-09-25 22:24:36 +0200

    completed several methods. Implemented the constructor. Writinf the access mehtod

--- Assignment1/pathORAM/src/pathOramHw/ORAMWithReadPathEviction.java
+++ Assignment1/pathORAM/src/pathOramHw/ORAMWithReadPathEviction.java
@@ -1,6 +1,8 @@
 package pathOramHw;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /*
  * Name: TODO
@@ -8,21 +10,52 @@ import java.util.ArrayList;
  */
 
 public class ORAMWithReadPathEviction implements ORAMInterface{
-	
-	/**
-	 * TODO add necessary variables 
-	 */
+
+	/** variable to store the number of blocks*/
 	private int num_blocks;
-	
+	/** variable to store the server storage*/
+	private UntrustedStorageInterface storage;
+	/** variable to store the positon map */
+	private int[] positionMap;
+	/** local stash variable */
+	private ArrayList<Block> stash;
+	/** variable to store the random number generator */
+	private RandomForORAMHW rand_gen;
+
+	/**
+	 * Constructs a new ORAM and initializes needed variables.
+	 * @param storage Server storage
+	 * @param rand_gen random number generator
+	 * @param bucket_size size of the buckets in the tree
+	 * @param num_blocks the number of blocks in the tree
+	 */
 	public ORAMWithReadPathEviction(UntrustedStorageInterface storage, RandForORAMInterface rand_gen, int bucket_size, int num_blocks){
-		// TODO complete the constructor
 		this.num_blocks = num_blocks;
+		this.storage = storage;
+		this.storage.setCapacity(getNumBuckets());
+		this.rand_gen = (RandomForORAMHW) rand_gen;
+		/* Set bound size for the random int generator */
+		rand_gen.setBound(getNumLeaves());
+		/* create and fill position map of size L*/
+		positionMap = new int[getNumLevels()];
+		for (int i = 0; i < positionMap.length; i++){
+			positionMap[i] = rand_gen.getRandomLeaf();
+		}
+		/* initialize the stash and leave it empty */
+		this.stash = new ArrayList<>();
+
 	}
 
 
 	@Override
 	public byte[] access(Operation op, int blockIndex, byte[] newdata) {
-		// TODO Must complete this method for submission
+		/* Remap block at blockIndex.*/
+		// get leaf where the data is stores
+		int x =  positionMap[blockIndex];//Arrays.binarySearch(positionMap, blockIndex);
+		// generate new position
+		int new_x = rand_gen.getRandomLeaf();
+		/* Read path for leaf */
+		// fill stash
 		return null;
 	}
 
@@ -36,22 +69,19 @@ public class ORAMWithReadPathEviction implements ORAMInterface{
 
 	@Override
 	public int[] getPositionMap() {
-		// TODO Must complete this method for submission
-		return null;
+		return positionMap;
 	}
 
 
 	@Override
 	public ArrayList<Block> getStash() {
-		// TODO Must complete this method for submission
-		return null;
+		return stash;
 	}
 
 
 	@Override
 	public int getStashSize() {
-		// TODO Must complete this method for submission
-		return 0;
+		return stash.size();
 	}
 
 	@Override
@@ -70,7 +100,7 @@ public class ORAMWithReadPathEviction implements ORAMInterface{
 
 	@Override
 	public int getNumBlocks() {
-		return this.num_blocks;
+		return num_blocks;
 	}
 
 
