730bb862c4990d75a93ebc51a43e4ec132b112e1
Raphael <19925795+RaphEnio@users.noreply.github.com>
2020-09-28 15:28:29 +0200

    changed  calculate_prob and getBiggerStash method

--- Assignment1/createGraph.py
+++ Assignment1/createGraph.py
@@ -2,20 +2,22 @@ from matplotlib import pyplot as plt
 import pathlib
 import numpy as np
 
-def calculate_prob(v, numberAccesses):
-    result = np.zeros(v.shape[0])
-    for i in range(v.shape[0]):
-        result[i] = (v[i]/numberAccesses)
+def calculate_prob(data, numAccess):
+    numStashes = data.shape[0]
+    result = np.zeros(numStashes)
+    for i in range(numStashes):
+        result[i] = (data[i]/numAccess)
     return result
 
 def getBiggerStash(stashes):
-    result = np.zeros(len(stashes))
+    keys = list(stashes.keys())
+    result = np.zeros(len(keys))
     count = 0
-    for stash in stashes:
+    for i in range(0, len(keys)):
         tmp = 0
-        for stashes2 in stashes:
-            if stashes2 > stash:
-                tmp += 1
+        for j in range(0, len(keys)):
+            if int(keys[j]) > int(keys[i]):
+                tmp += stashes.get(keys[j])
         result[count] = tmp
         count += 1
     return result
@@ -30,23 +32,24 @@ with open(filepath, "r") as f:
     for line in f:
         stashSize = line.split(",")[0]
         ac = int(line.split(",")[1])
-        if (int(stashSize) == -1):
+        if (int(stashSize) == -1 or int(stashSize) == 0):
             continue
         if stashSize not in stash:
             stash.update({stashSize: ac})
         else:
             stash[stashSize] += ac
 
-stashvalues = list(stash.values())
-biggerStashes =  np.array(getBiggerStash(stashvalues))
-probabilities = calculate_prob(biggerStashes, sum(stashvalues))
+biggerStashes =  np.array(getBiggerStash(stash))
+probabilities = calculate_prob(biggerStashes, sum(stash.values()))
 x = list(stash.keys())
 
 fig = plt.figure(figsize=(30,8))
 plt.title("Probability that the stash size is larger")
 plt.xlabel("stash size")
 plt.ylabel("probabilty")
-plt.bar(x,np.array(probabilities))
+#plt.plot(x,np.array(probabilities)) # line plot
+plt.bar(x,np.array(probabilities)) # bar plot
 plt.xticks(x)
 plt.xticks(rotation = 90)
+plt.grid(axis="y")
 plt.savefig("graph.png")
