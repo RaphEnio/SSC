a2376ab52c30194eaf7ed3a7935f808043ad6620
Raphael <19925795+RaphEnio@users.noreply.github.com>
2020-10-30 14:51:55 +0100

    started task 2

--- Assignment2/task2.py
+++ Assignment2/task2.py
@@ -6,3 +6,17 @@
 
 
 import support_functions as sf
+
+data = sf.read_json(r"C:\Users\Raphael\github\SSC\Assignment2\json_files\swhe-task2.json")
+
+#get needed variables
+#convert list of binary values into an integer
+ciphertexts = data["Ciphertext Collection"]
+p = int(data["SWHE"]["sk"])
+
+
+c1 = int(ciphertexts[0]["Ciphertext"])
+
+m1 = sf.decrypt_message(c1,p)
+
+print(m1)
