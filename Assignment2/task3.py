8145b1728ff9922223f5544c1c84eceaf38af364
Raphael <19925795+RaphEnio@users.noreply.github.com>
2020-10-30 19:55:16 +0100

    finished task 3

--- Assignment2/task3.py
+++ Assignment2/task3.py
@@ -5,7 +5,7 @@
 ###############################################################
 
 # Note: For binary strings a and b the Hamming 
-# distance is equal to the number of ones (population count) in a XOR b
+# distance is equal to the number of ones (population count) in a XOR b (wiki)
 
 # So to calculate hamming distance, calculate XOR. Only problem, XOR produces a lot of noise
 import support_functions as sf
@@ -14,13 +14,32 @@ import support_functions as sf
 data = sf.read_json(r"C:\Users\Raphael\github\SSC\Assignment2\json_files\swhe-task3_v2.json")
 
 #get needed variables
-pk = list(map(int, data["SWHE"]["Public Parameters"]["pk"])) 
+pk = list(map(int, data["SWHE"]["Public Parameters"]["pk"]))
+rho = int(data["SWHE"]["Public Parameters"]["rho"])
+tau = int(data["SWHE"]["Public Parameters"]["tau"]) 
 
 v1 = data["Ciphertext Collection"]["Encrypted V1"]
 v2 = data["Ciphertext Collection"]["Encrypted V2"]
 
-b1_0 = bin(int(v1[0]))
-b2_0 = bin(int(v2[0]))
 
-hamming_0 = bin(int(b1_0,2) ^ int(b2_0, 2))
-print(hamming_0.count('1'))
+#calculate the hamming distance between the individual ciphertexts of each collection
+final_distance = 0
+for i in range(0, len(v1)):
+    b1_i = bin(int(v1[i]))
+    b2_i = bin(int(v2[i]))
+    # add the distance to the total distance
+    # Use XOR and then count the number of ones
+    final_distance += bin(int(b1_i, 2) ^ int(b2_i, 2)).count('1')
+
+print("Hamming distance of ciphertexts: {}".format(final_distance))
+print("Hamming distance of ciphertexts (binary): {}".format(sf.decimalToBinary(final_distance)))
+
+
+# perform bitwise enryption of the hamming distance
+bin_hd = list(sf.decimalToBinary(final_distance))
+ciphertext_vector = []
+for bit in bin_hd:
+    cipher_bit = sf.encrypt_message(int(bit), rho, tau, pk)
+    ciphertext_vector.append(cipher_bit)
+
+print(ciphertext_vector)
